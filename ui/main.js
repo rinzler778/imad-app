b56497f1c97af593f1b165e5405a03cee0bb9f4e
rinzler778 <ajithphilip255@gmail.com>
2017-08-06 15:17:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -51,16 +51,35 @@ button.onclick = function (){
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
+
 submit.onclick = function(){
   
+    var request = new XMLHttpRequest();      //create a request object to counter end point
     
-    var names = ['name1', 'name2', 'name3'];
-    var list = '';
-    for(var i =0; i< names.length; i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ui = document.getElementById('nameList');
-    ui.innerHTML = list;
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        
+      if(request.readyState === XMLHttpRequest.DONE){
+
+          if(request.status === 200){    //request completed
+          
+            var names = request.resposeText;
+            names = JSON.parse(names);  //convert JSON to array
+            var list = '';
+            for(var i =0; i< names.length; i++){
+                list += '<li>' + names[i] + '</li>';
+            }
+            var ui = document.getElementById('nameList');
+            ui.innerHTML = list;
+            
+          }
+      }//not DONE yet
+    };
+    
+    
+    //make the request
+    request.open('GET', 'http://ajithphilip255.imad.hasura-app.io/submit-name?name='+ name , true);
+    request.send(null);
     
 };
 
