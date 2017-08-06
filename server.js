656753c57c79092e4f84805504c3534686c2de61
rinzler778 <ajithphilip255@gmail.com>
2017-08-06 13:20:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/', function (req, res) {
 });
 
 //add a counter
-var counter;
+var counter=0;
 app.get('/counter', function(req,res){
    counter = counter + 1;
    res.send(counter.toString());
