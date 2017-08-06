d543a0d6cb1f5e7e66071874a9994556f69d3f97
rinzler778 <ajithphilip255@gmail.com>
2017-08-06 12:56:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,13 @@
 console.log('Loaded!');
 
 //change main text in index.html
-
 var element = document.getElementById('mainText');
-
 element.innerHTML = 'New Value from main.js';
+
+//move image
+var img = document.getElementById('madi');
+img.onClick = function(){
+    img.stle.marginLeft = '100px';
+};
+
+
