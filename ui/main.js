a8a2c8cef525c167f29e004608021c94e72a715d
rinzler778 <ajithphilip255@gmail.com>
2017-08-06 14:47:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -54,7 +54,7 @@ var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
   
     
-    var names = ['name1', 'name1', 'name1'];
+    var names = ['name1', 'name2', 'name3'];
     var list = '';
     for(var i =0; i< names.length; i++){
         list += '<li>' + names[i] + '</li>';
