a520e3b033b5c4a464ac882e230cf84142d580bb
rinzler778 <ajithphilip255@gmail.com>
2017-08-06 13:50:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,4 +15,21 @@ img.onclick = function(){
     var interval = setInterval(moveRight, 50); //every 100ms call moveRight
 };*/
 
+//update counter text
+var button = document.getElementById('counterB');
+var counter = 0;
 
+button.onclick = function (){
+    
+    //make request to counter end point
+    
+    //capture the respnse and store it in a variable
+    
+    //render thevariable in the correct span
+    
+    counter = counter +1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+    
+}
