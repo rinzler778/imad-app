083564400baacf3addffa2f99a86895bc9b8b0f8
rinzler778 <ajithphilip255@gmail.com>
2017-08-06 15:19:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -69,7 +69,7 @@ submit.onclick = function(){
             for(var i =0; i< names.length; i++){
                 list += '<li>' + names[i] + '</li>';
             }
-            var ui = document.getElementById('nameList');
+            var ul = document.getElementById('nameList');
             ul.innerHTML = list;
             
           }
