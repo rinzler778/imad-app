db734aee75c5602beacb815f9b2d266876266ab3
rinzler778 <ajithphilip255@gmail.com>
2017-08-06 14:46:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -47,8 +47,22 @@ button.onclick = function (){
     
 };
 
-
-
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+  
+    
+    var names = ['name1', 'name1', 'name1'];
+    var list = '';
+    for(var i =0; i< names.length; i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ui = document.getElementById('nameList');
+    ui.innerHTML = list;
+    
+};
 
 
 
