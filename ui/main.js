d8fca93ebe8a4c4d191a5986bd775de4845dc992
rinzler778 <ajithphilip255@gmail.com>
2017-08-06 14:10:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,21 +15,50 @@ img.onclick = function(){
     var interval = setInterval(moveRight, 50); //every 100ms call moveRight
 };*/
 
+
 //update counter text
 var button = document.getElementById('counterB');
 var counter = 0;
 
 button.onclick = function (){
     
-    //make request to counter end point
+    //create a request object to counter end point
+    var request = new XMLHttpRequest();
+    
     
     //capture the respnse and store it in a variable
+    request.onreadystatechange = function(){
+      if(request.readystate === XMLHttpRequest.DONE){
+          //take some action
+          if(reuest.status === 200){    //request completed
+              var counter = request.responseText;   //extract value from request
+              //render the variable in the correct span
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      }
+      //not DONE yet
+    };
     
-    //render thevariable in the correct span
     
-    counter = counter +1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    //make the request
+    request.open('GET', 'http://ajithphilip255.imad.hasura-app.io/counter', true);
+    request.send(null);
     
-    
-}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
