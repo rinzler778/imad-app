0ff88d458f2d17f19f2de3c40ef4ca17115701d6
rinzler778 <ajithphilip255@gmail.com>
2017-08-06 10:59:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
-var express = require('express');
-var morgan = require('morgan');
+var express = require('express');   //create the server handling and listening to servers
+var morgan = require('morgan');     //logs
 var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));//ui/index.html
+});
+
+app.get('/article-one', function(req, res){
+   res.send('Article one will de served here...'); 
+});
+
+// three more articles
+
+app.get('/article-one', function(req, res){
+   res.send('Article one will de served here...'); 
+});
+
+app.get('/article-two', function(req, res){
+   res.send('Article two will de served here...'); 
+});
+
+app.get('/article-three', function(req, res){
+   res.send('Article three will de served here...'); 
 });
 
 app.get('/ui/style.css', function (req, res) {
