73fddd6121b372561959193933c5c1aa97c31831
rinzler778 <ajithphilip255@gmail.com>
2017-08-11 22:23:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.get('/test-db', function(req, res){
     pool.query('SELECT * FROM test', function(err, result){
        if(err){
            
-           res.send('res.status(500).send(err.toString());');
+           res.status(500).send(err.toString());
        } else{
            
            res.send(JSON.stringify(result));
