9a25380385c5f0b47ebfa64f4bfca3afbd848c26
rinzler778 <25707136+rinzler778@users.noreply.github.com>
2017-08-14 21:39:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var morgan = require('morgan');     //logs
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('bodyParser');
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 var config = {
     user: 'ajithphilip255',
@@ -85,16 +87,31 @@ app.get('/hash/:input', function(req, res){
    res.send(hashedString);
 });
 
+app.post('/create-user', function(req, res){
+    
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    var salt = crypto.randomBytes(128).toString('hex');
+    var dbString = hash(password, salt);    
+    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function(err, result){
+          if(err){
+           res.status(500).send(err.toString());
+       } else{
+           res.send('User created '+username +'\n'+ password);
+       }
+    });
+});
+
+
 var pool = new Pool(config);
 app.get('/test-db', function(req, res){
     
     
     pool.query('SELECT * FROM test', function(err, result){
        if(err){
-           
            res.status(500).send(err.toString());
        } else{
-           
            res.send(JSON.stringify(result.rows));
        }
     });
