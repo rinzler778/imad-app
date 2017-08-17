2c3b65e9460db3ebdefe28f1fc282a71bd502b04
rinzler778 <25707136+rinzler778@users.noreply.github.com>
2017-08-17 21:59:55 +0530

    [imad-console] Updates ui/login-page.js

--- ui/login-page.js
+++ ui/login-page.js
@@ -1,1 +1,40 @@
 cosole.log("Login Page");
+
+
+//submit login
+
+var submit = document.getElementById('submit_btn');
+
+submit.onclick = function(){
+  
+    var request = new XMLHttpRequest();      //create a request object to counter end point
+    
+    
+    request.onreadystatechange = function(){
+        
+      if(request.readyState === XMLHttpRequest.DONE){
+
+          if(request.status === 200){    //request completed
+          
+            console.log("user logged in");
+            alert("logged in successfully");
+            
+          } else if(request.status === 403){
+              alert("username/password incorrect");
+          }else if(request.status === 500){
+              alert("something went wrong");
+          }
+      }//not DONE yet
+    };
+    
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    
+    //make the request
+    request.open('POST', 'http://ajithphilip255.imad.hasura-app.io/login', true);
+    request.send(JSON.stringify({username: username, password: password}));
+    
+};
