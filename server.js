d659a21f4e5965dbdc70e5fadecd96fd88a4a4f4
rinzler778 <25707136+rinzler778@users.noreply.github.com>
2017-08-17 22:27:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -212,6 +212,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'login-page.js'));
+});
+
 
 
 
