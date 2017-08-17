f573f9389d37f2c3927ba3037eb95ced20e3c706
rinzler778 <25707136+rinzler778@users.noreply.github.com>
2017-08-17 20:15:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,21 +87,21 @@ app.get('/hash/:input', function(req, res){
    res.send(hashedString);
 });
 
-// app.post('/create-user', function(req, res){
+app.post('/create-user', function(req, res){
     
-//     var username = req.body.username;
-//     var password = req.body.password;
+    var username = req.body.username;
+    var password = req.body.password;
     
-//     var salt = crypto.randomBytes(128).toString('hex');
-//     var dbString = hash(password, salt);    
-//     pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function(err, result){
-//           if(err){
-//           res.status(500).send(err.toString());
-//       } else{
-//           res.send('User created '+username +'\n'+ password);
-//       }
-//     });
-// });
+    var salt = crypto.randomBytes(128).toString('hex');
+    var dbString = hash(password, salt);    
+    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function(err, result){
+          if(err){
+          res.status(500).send(err.toString());
+      } else{
+          res.send('User created ' + username);
+      }
+    });
+});
 
 
 var pool = new Pool(config);
